bcdc573e2f183232580360e6e15bda330da42d67
Bryant Novas <bryant.novas@gmail.com>
2021-05-01 16:46:57 -0400

    changes made by Bryant

---
+++ app.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import matplotlib.pyplot as plt
+
+df = pd.read_excel('file.xlsx')
+st.write(df)
+
+columns = ["Please inform us of how we could make the aforementioned topic more understandable and beneficial to all program participants? ",\
+       "What is the reason for this rating?",\
+       "What is the reason for this rating?.1", 
+       "What is the reason for this rating?.2",
+       "What is the reason for this rating?.3",
+       "What is the reason for this rating?.4", 
+        "Please elaborate on the aforementioned external factors affecting your performance, attendance, and/or overall progression in the program. Let us know how we can help. ",
+        "Additional comments and/or notes:",
+        "Is there a fellow or TKH staff member you would like to shine a light on this week? If so, who are they and why? (you can also choose yourself if applicable)"]
+
+
+for col in columns:
+  plt.subplot()
+  df[f"response+{col}"].value_counts().plot.pie(figsize=(15,10))
+  st.pyplot()
